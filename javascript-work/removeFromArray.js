43b29ee92ce70fbcac699f6a6ff3cfc33ba02da3
baylerblanchard <baylerblanchard@gmail.com>
2023-09-03 10:31:46 -0500

    it worked

--- javascript-work/removeFromArray.js
+++ javascript-work/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function(string, num, num2, num3, num4) {
     let array = string;
     let newArray = [];
     for (let i = 0; i < array.length; i++) {
-        if (array[i] !== num, num2, num3, num4) {
+        if (array[i] !== num && array[i] !== num2 && array[i] !== num3 && array[i] !== num4) {
         newArray.push(array[i]);
         }
     }
