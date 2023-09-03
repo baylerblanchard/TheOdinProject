94f3e7dee44b8083cbc779e3df626dc1f51d94e2
baylerblanchard <baylerblanchard@gmail.com>
2023-09-03 10:22:37 -0500

    first test on lesson04 was good

--- javascript-work/removeFromArray.js
+++ javascript-work/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(string, num, num2, num3, num4) {
+    let array = string;
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== num, num2, num3, num4) {
+        newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
