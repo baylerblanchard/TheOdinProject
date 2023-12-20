2083593f77a636b9e5cb9832901dd8393ca679be
baylerblanchard <baylerblanchard@gmail.com>
2023-12-20 13:17:28 -0600

    made changes to the js

--- javascript-work/wikipedia-search/js/app.js
+++ javascript-work/wikipedia-search/js/app.js
@@ -7,28 +7,69 @@ searchTermElem.addEventListener('input', function (event) {
     search(event.target.value);
 });
 
-let timeoutId;
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args)
+        }, delay);
+    };
+};
 
-const search = (searchTerm) => {
-    // reset the previous timer
-    if (timeoutId) {
-        clearTimeout(timeoutId);
+const search = debounce(async (searchTerm) => {
+
+    // if the search term is removed, 
+    // reset the search result
+    if (!searchTerm) {
+        // reset the search result
+        searchResultElem.innerHTML = '';
+        return;
     }
 
-    // set up a new timer
-    timeoutId = setTimeout(async () => {
-        try {
-            const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info|extracts&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${searchTerm}`;
-            const response = await fetch(url);
-            const searchResults = await response.json();
+    try {
+        // make an API request
+        const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info|extracts&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${searchTerm}`;
+        const response = await fetch(url);
+        const searchResults = await response.json();
 
-            // show the search result in the console
-            console.log({
-                'term': searchTerm,
-                'results': searchResults.query.search
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }, 500);
+        // render search result
+        const searchResultHtml = generateSearchResultHTML(searchResults.query.search, searchTerm);
+
+        // add the search result to the searchResultElem
+        searchResultElem.innerHTML = searchResultHtml;
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+const stripHtml = (html) => {
+    let div = document.createElement('div');
+    div.innerHTML = html;
+    return div.textContent;
 };
+
+const highlight = (str, keyword, className = "highlight") => {
+    const hl = `<span class="${className}">${keyword}</span>`;
+    return str.replace(new RegExp(keyword, 'gi'), hl);
+};
+
+const generateSearchResultHTML = (results, searchTerm) => {
+    return results
+        .map(result => {
+            const title = highlight(stripHtml(result.title), searchTerm);
+            const snippet = highlight(stripHtml(result.snippet), searchTerm);
+
+            return `<article>
+                <a href="https://en.wikipedia.org/?curid=${result.pageid}">
+                    <h2>${title}</h2>
+                </a>
+                <div class="summary">${snippet}...</div>
+            </article>`;
+        })
+        .join('');
+}
